c7d738e4bd968d6a1b514678e2d39518b766e16e
SQuirkster <develop@spontaneousquirk.com>
2019-03-08 18:05:58 -0800

    Base Unity Project
    Initial setup

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	// Use this for initialization
+	// The Awake function is called on all objects in the Scene before any object's Start function is called. 
+	void Awake() {
+        
+    }
+
+	// Use this for initialization
+	//Start is called on the frame when a script is enabled just before any of the Update methods are called the first time.
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
